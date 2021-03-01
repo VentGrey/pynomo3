fd0d2759e37d1381b1d2a3c372b1e34a5e6f4ead
VentGrey <omarpurataf@gmail.com>
2021-02-28 21:05:33 -0600

    Corrected import path

--- pynomo/circ_scale.py
+++ pynomo/circ_scale.py
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from nomo_axis import *
+from .nomo_axis import *
 
 class Circ_Scale:
     """
